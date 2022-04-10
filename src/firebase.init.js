cf14ab5b25aa51cd11e3578f4feae7376162a1ef
laskor <al.moksud@gmail.com>
2022-04-10 03:28:49 -0400

    added

---
+++ src/firebase.init.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwZ1C3RuMq6l6WI86ZIEmj0GGingpErW4",
+    authDomain: "react-authentication-b4f21.firebaseapp.com",
+    projectId: "react-authentication-b4f21",
+    storageBucket: "react-authentication-b4f21.appspot.com",
+    messagingSenderId: "607367416459",
+    appId: "1:607367416459:web:35ec93b878e93de688e28e"
+  };
+
+  // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+ export default app;
