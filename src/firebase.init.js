867e29d4ceac5a82b19018f896f56d5e187462df
laskor <al.moksud@gmail.com>
2022-04-10 03:54:20 -0400

    react 18

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,3 +1,9 @@
+//  Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAwZ1C3RuMq6l6WI86ZIEmj0GGingpErW4",
     authDomain: "react-authentication-b4f21.firebaseapp.com",
