262a28a6236d42631460e910366d99e581f1cc62
laskor <al.moksud@gmail.com>
2022-04-10 21:52:01 -0400

    google sign in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import './App.css';
-import app from './firebase.init';
-import {getAuth} from 'firebase/auth';
+import "./App.css";
+import app from "./firebase.init";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
-  const googleSignIn = () =>{
-    
-  }
+  const [user, setUser] = useState([]);
+
+  const provider = new GoogleAuthProvider();
+
+  const googleSignIn = () => {
+
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      setUser(user);
+    })
+    .catch(error =>{
+      
+    })
+  };
+  
   return (
     <div className="App">
-      <button onClick={googleSignIn}>Sign in</button>
+      <button onClick={googleSignIn}>Google Sign in</button>
     </div>
   );
 }
